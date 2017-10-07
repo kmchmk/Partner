cb71655ab7fb67205a7ceb94460d52d87498f50a
kmchmk1026 <kmchmk@gmail.com>
2017-10-07 23:41:34 +0530

    Started developing a login screen.

--- App.js
+++ App.js
@@ -9,29 +9,46 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Button,
+  Alert
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//   'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//   'Shake or press menu button for dev menu\n' +
+//   '-by Chanaka-',
+// });
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Partner!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          Couldn't find a life partner yet?
         </Text>
         <Text style={styles.instructions}>
-          {instructions}
+          Login and enter your details. We will provide you a match.
         </Text>
+        <TextInput style={styles.textinput}
+          placeholder="Email"
+        />
+        <TextInput style={styles.textinput}
+          placeholder="Password"
+        />
+        <Button
+          onPress={showAlert}
+          title="Login"
+          color="#841584"
+          accessibilityLabel="Click here to login"
+        />
+
       </View>
     );
   }
@@ -54,4 +71,21 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  textinput: {
+    height: 40,
+    width: 100,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
 });
+
+// Works on both iOS and Android
+const showAlert = () => Alert.alert(
+  'Alert',
+  'Please press OK',
+  [
+    { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+    { text: 'OK', onPress: () => console.log('OK Pressed') },
+  ],
+  { cancelable: false }
+)
